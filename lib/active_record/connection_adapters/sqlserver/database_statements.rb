9e05cf8705d2cf5a8f16d96a9caff47ad1b45c56
Anthony Garcia <agarcia@ncsasports.org>
2015-09-21 11:37:44 -0500

    Incorporate fix for https://github.com/rails-sqlserver/activerecord-sqlserver-adapter/issues/381.
    This corrects an error when trying to insert an Address, observed in
    https://ncsasports.atlassian.net/browse/PREM-340. It's an instance of
    this issue: https://github.com/rails-sqlserver/activerecord-sqlserver-adapter/issues/339

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -205,6 +205,15 @@ module ActiveRecord
           sql = "#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident"
           super
         end
+        def sql_for_insert(sql, pk, id_value, sequence_name, binds)
+          sql = if pk && self.class.use_output_inserted
+            quoted_pk = SQLServer::Utils.extract_identifiers(pk).quoted
+            sql.insert sql.index(/ (DEFAULT )?VALUES/), " OUTPUT INSERTED.#{quoted_pk}"
+          else
+            "#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident"
+          end
+          super
+        end
         # === SQLServer Specific ======================================== #
 
         def set_identity_insert(table_name, enable = true)
