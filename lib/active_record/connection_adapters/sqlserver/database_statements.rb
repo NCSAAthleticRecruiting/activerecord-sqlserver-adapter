6eef823e987d2756cfbd6b3c5d51a2dcdeae19d9
Anthony Garcia <agarcia@ncsasports.org>
2015-09-22 12:31:01 -0500

    Delete old sql_for_insert method.

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -200,10 +200,6 @@ module ActiveRecord
         end
 
         def sql_for_insert(sql, pk, id_value, sequence_name, binds)
-          sql = "#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident"
-          super
-        end
-        def sql_for_insert(sql, pk, id_value, sequence_name, binds)
           sql = if pk && self.class.use_output_inserted
             quoted_pk = SQLServer::Utils.extract_identifiers(pk).quoted
             sql.insert sql.index(/ (DEFAULT )?VALUES/), " OUTPUT INSERTED.#{quoted_pk}"
